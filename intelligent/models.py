a098bdb1e763560fc352ee8145ce975eac4ca6c7
CarolZhang509 <zhangcarol210@gmail.com>
2024-06-12 17:00:58 +0800

    增加监控视频，修改为鱼类视频

--- intelligent/models.py
+++ intelligent/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+# class WaterQualityPredict(models.Model):
+#     TEMP = models.FloatField()
+#     PH = models.FloatField()
+#     DO = models.FloatField()
+#     Cond = models.FloatField()
+#     NTU = models.FloatField()
+#     Mn= models.FloatField()
+#     NH3 = models.FloatField()
+
+#     def __str__(self):
+#         return self.species
